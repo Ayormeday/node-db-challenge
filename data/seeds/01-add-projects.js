fc01bb71b0603a7134015594d01d0b641d98c488
Ayomide Jones <ayormeday@gmail.com>
2019-11-08 11:49:18 +0100

    seed files created

--- data/seeds/01-add-projects.js
+++ data/seeds/01-add-projects.js
@@ -3,7 +3,16 @@ exports.seed = function(knex) {
     .truncate()
     .then(function() {
       return knex("projects").insert([
-        { project_name: "egg", project_description: "", project_status: "" },
+        { 
+          project_name: "School Library", 
+          project_description: "Proposed site to construct a school library", 
+          project_status: "false" 
+        },
+        { 
+          project_name: "Lambda School Sprints", 
+          project_description: "Complete the db sprint challenge in 3 hours", 
+          project_status: "false" 
+        },
       
         
       ]);
